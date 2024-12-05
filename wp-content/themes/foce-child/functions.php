b40a0f66f03638505ffd4265ccd91e1ba863a86a
ilydane <leoo75018@gmail.com>
2024-12-05 20:12:51 +0100

    animfleur suite

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -2,6 +2,7 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') ); // Charger le style du thème enfant
 }
 
 // Get customizer options form parent theme
@@ -14,3 +15,5 @@ if ( get_stylesheet() !== get_template() ) {
         return get_option( 'theme_mods_' . get_template(), $default );
     } );
 }
+
+
